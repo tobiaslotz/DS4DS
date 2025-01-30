ac61a91bc023cd990f15bdf6cc173da4bb9a9b3c
tobiaslotz <tobias.lotz@tu-dortmund.de>
2025-01-30 03:54:33 +0100

    updated experiment

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -28,6 +28,12 @@ X1 = torch.stack([X1_CH[i:i+100, :] for i in range(1, 301)])
 X2 = torch.stack([X2_CH[i:i+100, :] for i in range(1, 301)])
 X3 = torch.stack([X3_CH[i:i+100, :] for i in range(1, 301)])
 
+# merge all datasets
+X1X2X3_train = torch.vstack([X1[:TRAIN_INDEX, :, :], X2[:TRAIN_INDEX, :, :], X3[:TRAIN_INDEX, :, :]])
+X1X2X3_test = torch.vstack([X1[TRAIN_INDEX:, :, :], X2[TRAIN_INDEX:, :, :], X3[TRAIN_INDEX:, :, :]])
+y1y2y3_train = torch.vstack([y1[:TRAIN_INDEX, :], y2[:TRAIN_INDEX, :], y3[:TRAIN_INDEX, :]])
+y1y2y3_test = torch.vstack([y1[TRAIN_INDEX:, :], y2[TRAIN_INDEX:, :], y3[TRAIN_INDEX:, :]])
+
 # Shapes:
 # X: (300, 100, 10_000)
 # y: (300, 10_000)
@@ -42,6 +48,9 @@ class Trajectories(Dataset):
     
     def __getitem__(self, i):
         return self.X[i], self.y[i]
+    
+merged_train_set = Trajectories(X1X2X3_train, y1y2y3_train)
+merged_test_set = Trajectories(X1X2X3_test, y1y2y3_test)
 
 def create_train_test_loader(X, y, train_index=TRAIN_INDEX, batch_size=BATCH_SIZE):
     y_train = y[:train_index, :]
@@ -60,6 +69,8 @@ def create_train_test_loader(X, y, train_index=TRAIN_INDEX, batch_size=BATCH_SIZ
 X1_train_loader, X1_test_loader = create_train_test_loader(X1, y1)
 X2_train_loader, X2_test_loader = create_train_test_loader(X2, y2)
 X3_train_loader, X3_test_loader = create_train_test_loader(X3, y3)
+X1X2X3_train_loader = DataLoader(merged_train_set, batch_size=BATCH_SIZE, shuffle=True)
+X1X2X3_test_loader = DataLoader(merged_test_set, batch_size=BATCH_SIZE, shuffle=False)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -81,21 +92,19 @@ class PINN(nn.Module):
         return out
 
 
-def train_model(train_loader, test_loader, config, verbose=True):
+def train_model(train_loader, test_loader, config, dataset_name, verbose=True):
     model = PINN(config['hidden_size'], config['num_layers'])
     model.to(device)
-    best_state_dict = {}
     best_test_loss = 10_000
 
     loss_func = nn.MSELoss() # + PDE informed loss
-    #optimizer = torch.optim.AdamW(model.parameters(), lr=config['lr'])
     optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'])
 
     for epoch in range(NUM_EPOCHS):
         model.train(True)
-        #if verbose:
-        print(f"Starting epoch: {epoch + 1}")
-        print(f"*******************************")
+        if verbose:
+            print(f"Starting epoch: {epoch + 1}")
+            print(f"*******************************")
         running_loss = 0.0
         epoch_loss = 0.0
 
@@ -115,15 +124,15 @@ def train_model(train_loader, test_loader, config, verbose=True):
                 print(f"Batch {i}, Loss: {avg_loss : .3f}")
                 running_loss = 0.0
 
-        #if verbose:
-        print(f"*******************************")
+        if verbose:
+            print(f"*******************************")
         
         val_loss = eval_model(test_loader, model, loss_func, verbose=verbose)
         if val_loss < best_test_loss:
             best_test_loss = val_loss
-            best_state_dict = model.state_dict()
-    
-    return PINN(config['hidden_size'], config['num_layers']).load_state_dict(best_state_dict), best_test_loss
+            torch.save(model, f"LSTM_{dataset_name}")
+
+    return best_test_loss
 
 
 def eval_model(test_loader, model, loss_func, verbose=True):
@@ -146,54 +155,55 @@ def eval_model(test_loader, model, loss_func, verbose=True):
     return avg_loss
 
 
-def main(use_x1=False, use_x2=False, use_x3=False):
-    if not use_x1 and not use_x2 and not use_x3:
+def main(use_x1=False, use_x2=False, use_x3=False, use_all=False):
+    if not use_x1 and not use_x2 and not use_x3 and not use_all:
         return
-    best_loss_x1 = best_loss_x2 = best_loss_x3 = 10_000
-    best_config_x1 = best_config_x2 = best_config_x3 = {}
-    best_model_x1 = best_model_x2 = best_model_x3 = None
-    param_space = list(product([5e-3, 1e-3, 1e-2], [5096, 4096, 2048, 1024], [1, 2]))
-    #param_space = [(1e-3, 5096, 1)]
-    #param_space = [(1e-3, 4048, 1)]
+    best_loss_x1 = best_loss_x2 = best_loss_x3 = best_loss_all = 10_000
+    best_config_x1 = best_config_x2 = best_config_x3 = best_config_all = {}
+    #param_space = list(product([5e-3, 1e-3, 1e-2], [5096, 4096, 2048, 1024], [1, 2]))
+    param_space = [(1e-3, 2048, 1)]
     for lr, hs, nl in tqdm(param_space):
         param_config = {"lr": lr, "hidden_size" : hs, "num_layers" : nl}
         
         if use_x1:
-            model_x1, loss_x1 = train_model(X1_train_loader, X1_test_loader, param_config, verbose=True)
+            loss_x1 = train_model(X1_train_loader, X1_test_loader, param_config, "X1", verbose=True)
         if use_x2:
-            model_x2, loss_x2 = train_model(X2_train_loader, X2_test_loader, param_config, verbose=True)
+            loss_x2 = train_model(X2_train_loader, X2_test_loader, param_config, "X2", verbose=True)
         if use_x3:
-            model_x3, loss_x3 = train_model(X3_train_loader, X3_test_loader, param_config, verbose=True)
+            loss_x3 = train_model(X3_train_loader, X3_test_loader, param_config, "X3", verbose=True)
+        if use_all:
+            loss_all = train_model(X1X2X3_train_loader, X1X2X3_test_loader, param_config, "X1X2X3", verbose=True)
         
         if use_x1 and loss_x1 < best_loss_x1:
             best_loss_x1 = loss_x1
-            best_model_x1 = model_x1
             best_config_x1 = param_config
         if use_x2 and loss_x2 < best_loss_x2:
             best_loss_x2 = loss_x2
-            best_model_x2 = model_x2
             best_config_x2 = param_config
         if use_x3 and loss_x3 < best_loss_x3:
             best_loss_x3 = loss_x3
-            best_model_x3 = model_x3
             best_config_x3 = param_config
+        if use_all and loss_all < best_loss_all:
+            best_loss_all = loss_all
+            best_config_all = param_config
 
     if use_x1:
-        print(f"X1 ==> best config: {best_config_x1}")
-        torch.save(best_model_x1, "LSTM_X1")
+        print(f"X1 ==> best config: {best_config_x1}   test loss ==> {best_loss_x1 : .3f}")
 
     if use_x2:
-        print(f"X2 ==> best config: {best_config_x2}")
-        torch.save(best_model_x2, "LSTM_X2")
+        print(f"X2 ==> best config: {best_config_x2}   test loss ==> {best_loss_x2 : .3f}")
 
     if use_x3:
-        print(f"X3 ==> best config: {best_config_x3}")
-        torch.save(best_model_x3, "LSTM_X3")
+        print(f"X3 ==> best config: {best_config_x3}   test loss ==> {best_loss_x3 : .3f}")
+
+    if use_all:
+        print(f"X1X2X3 ==> best config: {best_config_all}   test loss ==> {best_loss_all : .3f}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--X1", action="store_true")
     parser.add_argument("--X2", action="store_true")
     parser.add_argument("--X3", action="store_true")
+    parser.add_argument("--X1X2X3", action="store_true")
     args = parser.parse_args()
-    main(args.X1, args.X2, args.X3)
+    main(args.X1, args.X2, args.X3, args.X1X2X3)
