d1c46c4f4aef7f2a0e70c1f9bf1afb200124bca9
tobiaslotz <tobias.lotz@tu-dortmund.de>
2025-02-03 14:12:37 +0100

    extended experiment

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
 import torch 
 import torch.nn as nn
-import torch.nn.functional as f
+import torch.nn.functional as F
 import argparse
 #import matplotlib.pyplot as plt
 #from ray import tune
@@ -14,7 +14,7 @@ from tqdm import tqdm
 data = h5py.File("data/CH_D3.jld2", "r")
 TRAIN_INDEX = 260
 BATCH_SIZE = 5
-NUM_EPOCHS = 10
+NUM_EPOCHS = 15
 X1_CH = torch.Tensor(np.array(data["X1"]))
 X2_CH = torch.Tensor(np.array(data["X2"]))
 X3_CH = torch.Tensor(np.array(data["X3"]))
@@ -88,11 +88,33 @@ class PINN(nn.Module):
 
     def forward(self, x):
         out, _ = self.lstm(x, (self.h0, self.c0))
-        out = f.tanh(self.fc(out[:,-1,:]))
+        out = F.tanh(self.fc(out[:,-1,:]))
         return out
+    
+def laplacian_2d(x, grid_size):
+    """
+    Approximation des Laplace-Operators mit einer 5-Punkt-Diskretisierung.
+    """
+    x = x.view(-1, grid_size, grid_size)
+    laplace_x = (
+        -4 * x +
+        torch.roll(x, shifts=1, dims=1) + torch.roll(x, shifts=-1, dims=1) +
+        torch.roll(x, shifts=1, dims=2) + torch.roll(x, shifts=-1, dims=2)
+        ) / (grid_size**2)
+    return laplace_x.view(-1, grid_size * grid_size)
 
+def cahn_hilliard(x, grid_size, D=3, gamma=0.5):
+    laplace_x = laplacian_2d(x, grid_size)
+    laplace_x3 = laplacian_2d(x**3, grid_size)
+    laplace_laplace_x = laplacian_2d(laplace_x, grid_size)
+    return D * (laplace_x3 - laplace_x - gamma * laplace_laplace_x)
 
-def train_model(train_loader, test_loader, config, dataset_name, verbose=True):
+def pinn_loss(y_true, y_pred, grid_size=100):
+    mse_loss = F.mse_loss(y_pred, y_true)
+    ch_loss = F.mse_loss(cahn_hilliard(y_pred, grid_size), cahn_hilliard(y_true, grid_size))
+    return mse_loss + ch_loss
+
+def train_model(train_loader, test_loader, config, dataset_name, verbose=True, use_pinn=False, num_epochs=NUM_EPOCHS):
     model = PINN(config['hidden_size'], config['num_layers'])
     model.to(device)
     best_test_loss = 10_000
@@ -100,7 +122,7 @@ def train_model(train_loader, test_loader, config, dataset_name, verbose=True):
     loss_func = nn.MSELoss() # + PDE informed loss
     optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'])
 
-    for epoch in range(NUM_EPOCHS):
+    for epoch in range(num_epochs):
         model.train(True)
         if verbose:
             print(f"Starting epoch: {epoch + 1}")
@@ -109,15 +131,22 @@ def train_model(train_loader, test_loader, config, dataset_name, verbose=True):
         epoch_loss = 0.0
 
         for i, batch in enumerate(train_loader):
-            X_batch, y_batch = batch[0].to(device), batch[1].to(device)
+            if use_pinn:
+                X_batch, y_batch = torch.autograd.Variable(batch[0].to(device), requires_grad=True), batch[1].to(device)
+            else:
+                X_batch, y_batch = batch[0].to(device), batch[1].to(device)
             y_pred = model(X_batch)
             loss = loss_func(y_pred, y_batch)
-            running_loss += loss
-            epoch_loss += loss
-
             optimizer.zero_grad()
+            if use_pinn:
+                loss.backward(retain_graph=True)
+                dxdt = X_batch.grad[:,-1,:]
+                loss += ((dxdt - cahn_hilliard(y_pred, 100))**2).mean()
+                optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            running_loss += loss
+            epoch_loss += loss
 
             if i % 10 == 0 and i > 0 and verbose:
                 avg_loss = running_loss / 10
@@ -130,7 +159,10 @@ def train_model(train_loader, test_loader, config, dataset_name, verbose=True):
         val_loss = eval_model(test_loader, model, loss_func, verbose=verbose)
         if val_loss < best_test_loss:
             best_test_loss = val_loss
-            torch.save(model, f"LSTM_{dataset_name}")
+            if use_pinn:
+                torch.save(model, f"PINN_{dataset_name}")
+            else:
+                torch.save(model, f"LSTM_{dataset_name}")
 
     return best_test_loss
 
@@ -155,7 +187,7 @@ def eval_model(test_loader, model, loss_func, verbose=True):
     return avg_loss
 
 
-def main(use_x1=False, use_x2=False, use_x3=False, use_all=False):
+def main(use_x1=False, use_x2=False, use_x3=False, use_all=False, use_pinn=False, num_epochs=NUM_EPOCHS):
     if not use_x1 and not use_x2 and not use_x3 and not use_all:
         return
     best_loss_x1 = best_loss_x2 = best_loss_x3 = best_loss_all = 10_000
@@ -166,13 +198,17 @@ def main(use_x1=False, use_x2=False, use_x3=False, use_all=False):
         param_config = {"lr": lr, "hidden_size" : hs, "num_layers" : nl}
         
         if use_x1:
-            loss_x1 = train_model(X1_train_loader, X1_test_loader, param_config, "X1", verbose=True)
+            loss_x1 = train_model(X1_train_loader, X1_test_loader, 
+                                  param_config, "X1", verbose=True, use_pinn=use_pinn, num_epochs=num_epochs)
         if use_x2:
-            loss_x2 = train_model(X2_train_loader, X2_test_loader, param_config, "X2", verbose=True)
+            loss_x2 = train_model(X2_train_loader, X2_test_loader, 
+                                  param_config, "X2", verbose=True, use_pinn=use_pinn, num_epochs=num_epochs)
         if use_x3:
-            loss_x3 = train_model(X3_train_loader, X3_test_loader, param_config, "X3", verbose=True)
+            loss_x3 = train_model(X3_train_loader, X3_test_loader, 
+                                  param_config, "X3", verbose=True, use_pinn=use_pinn, num_epochs=num_epochs)
         if use_all:
-            loss_all = train_model(X1X2X3_train_loader, X1X2X3_test_loader, param_config, "X1X2X3", verbose=True)
+            loss_all = train_model(X1X2X3_train_loader, X1X2X3_test_loader, 
+                                   param_config, "X1X2X3", verbose=True, use_pinn=use_pinn, num_epochs=num_epochs)
         
         if use_x1 and loss_x1 < best_loss_x1:
             best_loss_x1 = loss_x1
@@ -205,5 +241,7 @@ if __name__ == '__main__':
     parser.add_argument("--X2", action="store_true")
     parser.add_argument("--X3", action="store_true")
     parser.add_argument("--X1X2X3", action="store_true")
+    parser.add_argument("--pinn", action="store_true")
+    parser.add_argument("--n_epochs", type=int, default=15)
     args = parser.parse_args()
-    main(args.X1, args.X2, args.X3, args.X1X2X3)
+    main(args.X1, args.X2, args.X3, args.X1X2X3, args.pinn, args.n_epochs)
